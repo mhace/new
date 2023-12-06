2369bcdcad62ff7f2e9cd2717ddf3ab3ab40bde9
gaylebonavente <2191655@slu.edu.ph>
2023-12-06 22:19:38 +0800

    renamed db in db.php

--- php/db.php
+++ php/db.php
@@ -2,7 +2,7 @@
     $host = 'localhost';
     $user = 'root';
     $password = '';
-    $database = 'db';
+    $database = 'teamtwoone-final';
 
     $conn = mysqli_connect($host, $user, $password,$database);
 
