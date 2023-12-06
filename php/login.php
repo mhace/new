5d49f05d20d8b56b09e02ef1032ed20e66ad5ff9
Chelsy-Joy <143395420+Chelsy-Joy@users.noreply.github.com>
2023-12-06 22:25:00 +0800

    update db name

--- php/login.php
+++ php/login.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $servername = "localhost"; 
     $username = "root"; 
     $password = ""; 
-    $database = "document_db";
+    $database = "teamtwoone-final";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
