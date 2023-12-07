cb81bfd16e957df5f3912792bb17670ada342786
mhace <37394580+mhace@users.noreply.github.com>
2023-12-07 21:02:13 +0800

    Add files via upload

--- php/admin.php
+++ php/admin.php
@@ -1,15 +1,19 @@
 <?php 
-    include('includes db.php');
+    include 'db.php';
+    echo"<script>console.log('aw')</script>";
+    echo isset($_POST);
 
-    if (isset($_POST['add'])){
+    if (isset($_POST)){
+        echo"<script>console.log('true')</script>";
         $role = $_POST['role'];
         $username = $_POST['username'];
         $password = $_POST['password'];
         $first_name = $_POST['first_name'];
         $last_name =$_POST['last_name'];
 
-    $addUsers = "INSERT INTO admin value (null,'$username','$password','$first_name','$last_name')";
+    $addUsers = "INSERT INTO users value (NULL,'$username','$password',1)";
     $sqlCreate = mysqli_query($conn, $addUsers);
+    echo $sqlCreate;
 
     echo '<script>alert("Succesfully Created!")</script>';
     echo '<script>window.location.href = "/pages/adminUsers.html"</script>';
