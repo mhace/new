5639559ecbf61cf968e8b0675fe535ead2f5e946
mace <2144792@slu.edu.ph>
2023-12-11 20:06:57 +0800

    Remove file limitations

--- upload.js
+++ upload.js
@@ -15,36 +15,8 @@ const upload = multer({ storage: storage });
 
 const uploadMiddleware = (req, res, next) => {
   upload.array('files', 5)(req, res, (err) => {
-    if (err) {
-      return res.status(400).json({ error: err.message });
-    }
-
     const files = req.files;
-    const errors = [];
-
-    files.forEach((file) => {
-      const allowedTypes = ['image/jpeg', 'image/png'];
-      const maxSize = 5 * 1024 * 1024; // 5MB
-
-      if (!allowedTypes.includes(file.mimetype)) {
-        errors.push(`Invalid file type: ${file.originalname}`);
-      }
-
-      if (file.size > maxSize) {
-        errors.push(`File too large: ${file.originalname}`);
-      }
-    });
-
-    if (errors.length > 0) {
-      files.forEach((file) => {
-        fs.unlinkSync(file.path);
-      });
-
-      return res.status(400).json({ errors });
-    }
-
     req.files = files;
-
     next();
   });
 };
